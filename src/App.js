763e8ba9b6eeaadf45eb450bc83e9935879e77c8
tranchi0120 <tranchi20012000@gmail.com>
2023-03-25 17:24:08 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Companies from "./Components/Companies";
+import Details from "./Components/Details";
+import Footer from "./Components/Footer";
+import GetStarted from "./Components/GetStarted";
+import Guide from "./Components/Guide";
+import Hero from "./Components/Hero";
+import Properties from "./Components/Properties";
+import ScrollReveal from "scrollreveal";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const sr = ScrollReveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 1500,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        #hero,
+        #companies,
+        #guide,
+        #properties,
+        #details,
+        #getstarted,
+        #getstarted-img,
+        #footer
+        `,
+      {
+        opacity: 0,
+        interval: 300,
+      }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hero />
+      <Companies />
+      <Guide />
+      <Properties />
+      <Details />
+      <GetStarted />
+      <Footer />
+    </>
   );
 }
 
